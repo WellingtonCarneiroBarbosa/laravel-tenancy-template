8c6f9428718c96c48decb9c018533a7ca5e98357
wellington barbosa <wellingtonbarbosa.dev@gmail.com>
2024-01-12 19:48:22 -0300

    fix layouts

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -520,7 +520,11 @@ onMounted(() => {
 
             <!-- Page Content -->
             <main>
-                <slot />
+                <div class="py-5 md:py-10">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <slot />
+                    </div>
+                </div>
             </main>
         </div>
 
